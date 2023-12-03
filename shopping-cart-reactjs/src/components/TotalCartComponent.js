d5d97f5dc4c1c343fa3679f8584380944827431f
Karunya <Karunya.g@gmail.com>
2023-12-03 22:04:21 +0530

    style changes for price summary

--- shopping-cart-reactjs/src/components/TotalCartComponent.js
+++ shopping-cart-reactjs/src/components/TotalCartComponent.js
@@ -17,31 +17,41 @@ function TotalCartComponent({
         <h2>Price details</h2>
         <div className="total">
             <span> Price: </span>
-            <span className="totalValue"> ₹{totalAmountCalculationFunction()}</span>
+            </div>
+            <div className="total1">
+            <span>  ₹{totalAmountCalculationFunction()} </span>
             </div>
         
             <div className="total">
             <span> Discount: </span> 
+            </div>
+            <div className="total1">
             <span> 5% </span> 
             </div>
 
             <div className="total">
                 <span> shipping charges: </span>
+                </div>
+                <div className="total1">
                 <span> Free </span>
             </div>
 
             <div className="total">
                 <span>Taxes: </span>  
+                </div>
+                <div className="total2">
                  <span> Calculated at checkout </span>
                </div>
             
             
-            <div className="checkout-section">
-            <p className="pricelist"><h2>Estimated total:</h2>
-                ₹{totalAmountCalculationFunction()}
-            </p>
-            
+            <div className="total">
+            <span> <h3>Estimated total:</h3> </span>
             </div>
+            <div className="total3">
+            <span>   ₹{totalAmountCalculationFunction() } </span>
+            </div>
+
+
         <button
             className="checkout-button"
             disabled={cartCourses.length === 0 ||
